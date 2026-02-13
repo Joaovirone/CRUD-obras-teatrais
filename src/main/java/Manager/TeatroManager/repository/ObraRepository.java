2b3b8cb5dd417302fed32812708b930c80059095
joao <jovmamikl@gmail.com>
2026-02-13 07:56:35 -0300

    feat: Implementação de métodos específicos para o Repository da Obra:
    Consultar Obras por nome, diretor e data assistida

--- src/main/java/Manager/TeatroManager/repository/ObraRepository.java
+++ src/main/java/Manager/TeatroManager/repository/ObraRepository.java
@@ -1,7 +1,7 @@
 package Manager.TeatroManager.repository;
 
 import java.util.*;
-
+import java.time.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import Manager.TeatroManager.entity.Obra;
@@ -9,4 +9,8 @@ import Manager.TeatroManager.entity.Obra;
 public interface ObraRepository extends JpaRepository<Obra, UUID> {
     
     List<Obra> findByNomeContainigIgnoreCase(String nome);
+
+    List<Obra> findObraByDate(LocalDate dataAssistida);
+
+    List<Obra> findObraByNomeDiretor(String diretor);
 }
