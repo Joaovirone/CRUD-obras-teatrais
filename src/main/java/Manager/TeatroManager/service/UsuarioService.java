fc301f922dcbcdb9e02d835a392515b4b481d75f
joaovirone <jovmamikl@gmail.com>
2026-02-14 19:44:01 -0300

    refactor: implementado métodos para atualizar senha, buscar usuario por email e login de usuario como USER utilizando criptografia para senha

--- src/main/java/Manager/TeatroManager/service/UsuarioService.java
+++ src/main/java/Manager/TeatroManager/service/UsuarioService.java
@@ -5,41 +5,83 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import Manager.TeatroManager.entity.Usuario;
+import Manager.TeatroManager.exception.EntityNotFoundException;
+import Manager.TeatroManager.exception.PasswordInvalidException;
 import Manager.TeatroManager.exception.UsernameUniqueViolationException;
 import Manager.TeatroManager.repository.UsuarioRepository;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
+
+
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService{
     
     @Autowired
     private UsuarioRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Transactional
     public Usuario criarUsuario(Usuario usuario){
 
         try {
+
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
             return repository.save(usuario);
         } catch (DataIntegrityViolationException e) {
             throw new UsernameUniqueViolationException((String.format("O email | %s | -- Já está cadastrado no banco de dados.", usuario)));
         }
     }
 
-    @Transactional
-    public List<Usuario> buscarUsuarioPorUsername(String username){
 
-        try {
-            return repository.findUsuarioByUsername(username);
-        } catch (Exception e) {
-            throw new EntityNotFoundException((String.format("O email | %s | -- Não foi encontrado", username)));
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Usuario usuario = repository.findUsuarioByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+                
+        return org.springframework.security.core.userdetails.User
+                .withUsername(usuario.getUsername())
+                .password(usuario.getPassword())
+                .authorities("USER") 
+                .build();
+}
+
+
+    @Transactional(readOnly = true)
+    public Usuario buscarUsuarioPorUsername(String username){
+
+        return repository.findUsuarioByUsername(username)
+                .orElseThrow(()-> new EntityNotFoundException((String.format("O email | %s | -- Não foi encontrado no banco de dados", username))));
+
+    }
+
+    @Transactional
+    public Usuario atualizarSenha(String username, String senhaAtual, String novaSenha, String confirmarSenha){
+
+        if(!novaSenha.equals(confirmarSenha)){
+            throw new PasswordInvalidException("Nova senha não confere com a confirmação de senha!");
+
         }
 
+        Usuario usuario = this.buscarUsuarioPorUsername(username);
 
+        if (!passwordEncoder.matches(senhaAtual, usuario.getPassword())){
+
+            throw new PasswordInvalidException("Senha atual não confere!");
+        }
+
+        usuario.setPassword(passwordEncoder.encode(novaSenha));
+        return repository.save(usuario);
     }
 
 }
