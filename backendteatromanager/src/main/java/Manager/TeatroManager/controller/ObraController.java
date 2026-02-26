34b6aa658863d21f985078778c4c195755d6349f
joao <jovmamikl@gmail.com>
2026-02-26 08:18:34 -0300

    fix: removida importações inutiilizadas

--- backendteatromanager/src/main/java/Manager/TeatroManager/controller/ObraController.java
+++ backendteatromanager/src/main/java/Manager/TeatroManager/controller/ObraController.java
@@ -4,7 +4,6 @@ package Manager.TeatroManager.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.hibernate.metamodel.mapping.ordering.OrderByFragmentImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,9 +19,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 
